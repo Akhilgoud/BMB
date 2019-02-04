3e636b495b37253ba24ceb2527e569356156e072
Akhil Goud <akhilgoud616@gmail.com>
2019-02-04 21:30:53 +0530

    For Better Future

--- src/pages/myposts/myposts.service.ts
+++ src/pages/myposts/myposts.service.ts
@@ -23,14 +23,42 @@ export class MyPostsPageService {
         return this.booksList;
     }
 
+    // filterItems(ev: any) {
+    //     this.booksList = [...this.tempBooksList];
+    //     let val = ev.target.value;
+    //     if (val && val.trim() != '') {
+    //         this.booksList = this.booksList.filter((item) => {
+    //             return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    //         });
+    //     }
+    //     this.myBooksListChange.next(this.booksList);
+    // }
+
     filterItems(ev: any) {
         this.booksList = [...this.tempBooksList];
         let val = ev.target.value;
         if (val && val.trim() != '') {
             this.booksList = this.booksList.filter((item) => {
-                return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+                return (
+                    (item.name ? item.name.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.price ? item.price.toString().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.author ? item.author.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.bookType ? item.bookType.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.description ? item.description.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.edition ? item.edition.toString().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.publisher ? item.publisher.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.bookAcademic[0] && item.bookAcademic[0].branch ? item.bookAcademic[0].branch.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.bookAcademic[0] && item.bookAcademic[0].course ? item.bookAcademic[0].course.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.bookContact[0] && item.bookContact[0].address ? item.bookContact[0].address.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.bookContact[0] && item.bookContact[0].landmark ? item.bookContact[0].landmark.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.bookContact[0] && item.bookContact[0].college ? item.bookContact[0].college.toLowerCase().indexOf(val.toLowerCase()) > -1 : false) ||
+                    (item.bookContact[0] && item.bookContact[0].pincode ? item.bookContact[0].pincode.toString().indexOf(val.toLowerCase()) > -1 : false)
+                );
             });
         }
         this.myBooksListChange.next(this.booksList);
+
     }
+
+
 }
